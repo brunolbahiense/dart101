313157135876add2613d26e2264fc9f9c83383de
brunolbahiense <brunoluisbahiense@gmail.com>
2023-05-22 22:03:26 -0300

    separando arquivos

---
+++ bin/loops.dart
@@ -0,0 +1,7 @@
+import 'package:projeto_inicial/projeto_inicial.dart' as loops;
+
+void main() {
+  for(int i = 0; i <= 3; i++){
+    print(i);
+  }
+}
--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,7 +1,7 @@
 import 'package:projeto_inicial/projeto_inicial.dart' as projeto_inicial;
 
 void main() {
-
+  /// estrutura de infos
   const String nome = 'Bruno';
   int idade = 7;
   final double altura;
@@ -11,19 +11,19 @@ void main() {
 
   List<String> nomes = ['Milena', 'Bluis'];
   List<dynamic> infos = [nome, idade,altura, vivo, comparar];
-  String maiorDeIdade = 'menor de idade';
+  final String maiorDeIdade;
+
+
   if(idade >= 18){
     maiorDeIdade = 'maior de idade';
+  }else {
+    maiorDeIdade = 'menor de idade';
   }
 
 
   String frase = 'Sou o ${infos[0]} \n'
-      'tenho ${infos[1]} anos e '
+      'tenho ${infos[1]} anos, '
       '${infos[2]}cm de altura '
       'e sou $maiorDeIdade';
   print(frase);
-
-  for(int i = 0; i <= 3; i++){
-    print(i);
-  }
 }
